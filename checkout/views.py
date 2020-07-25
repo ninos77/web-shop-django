213c5095d5f3025248781fc539cbab0834f1015d
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-25 22:46:43 +0000

    Added stripe payment elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51H8vdOLJ0mxYFlet02PlGcAoac15cavNCK4CfkofpxCthsjgX82Wg2mWMg8ZflN2UZGlRGEFqzklwuNyW3RB88XG00amGM9diH',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
