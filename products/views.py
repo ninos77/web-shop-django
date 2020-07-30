591167a44a20e5bcb15d14ce64e59860eb653d96
Ninos Zaiya <ninos_zaiya@hotmail.com>
2020-07-30 23:01:28 +0000

    Product Admin - securing views

--- products/views.py
+++ products/views.py
@@ -4,6 +4,7 @@ from django.db.models import Q
 from .models import Product, Category
 from django.db.models.functions import Lower
 from products.forms import ProductForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -66,10 +67,13 @@ def product_detail(request, product_id):
     }
     return render(request, "products/product_detail.html", context)
 
-
+@login_required
 def add_product(request):
     """ Add a product to the store """
-    if request.method == "POST" :
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
+    if request.method == "POST":
         form = ProductForm(request.POST, request.FILES)
         if form.is_valid:
             product = form.save()
@@ -86,9 +90,12 @@ def add_product(request):
 
     return render(request, template, context)
 
-
+@login_required
 def edit_product(request, product_id):
     """ Edit a product in the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     if request.method == 'POST':
         form = ProductForm(request.POST, request.FILES, instance=product)
@@ -110,9 +117,12 @@ def edit_product(request, product_id):
 
     return render(request, template, context)
 
-
+@login_required
 def delete_product(request, product_id):
     """ Delete a product from the store """
+    if not request.user.is_superuser:
+        messages.error(request, 'Sorry, only store owners can do that.')
+        return redirect(reverse('home'))
     product = get_object_or_404(Product, pk=product_id)
     product.delete()
     messages.success(request, 'Product deleted!')
